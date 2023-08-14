f17f06c846abb77e5896d31d04c348c83e19d928
PK <pavak87@yandex.ru>
2023-08-14 18:56:50 +0300

    2

---
+++ 2.py
@@ -0,0 +1,10 @@
+a = list(map(int, input().split()))
+
+end = len(a) - 1
+for i in range(len(a)):
+        if a[i] > a[end]:
+            a[i], a[end] = a[end], a[i]
+            end -= end
+print(a)
+
+
